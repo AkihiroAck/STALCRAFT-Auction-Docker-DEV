57acdd7628c121e5140bd90c719c79489abf7bc0
AkihiroAck <canpolad99@gmail.com>
2025-09-20 04:07:34 +0500

    2025-09-20

--- backend/scaw/auction/constants.py
+++ backend/scaw/auction/constants.py
@@ -1,9 +1,11 @@
+# Первые 7 цвета используются и для редкости артефактов. По этому новые цвета добавлять нужно в конец
 RANK_COLORS = {
-    "DEFAULT": "#666666",
-    "RANK_NEWBIE": "#00c718",
-    "RANK_STALKER": "#4d66ff",
-    "RANK_VETERAN": "#e000be",
-    "RANK_MASTER": "#ff2c1a",
-    "RANK_LEGEND": "#f5b200",
-    "RANK_UNIQUE": "#b98600",
+    'DEFAULT': '#666666',
+    'RANK_NEWBIE': '#00c718',
+    'RANK_STALKER': '#4d66ff',
+    'RANK_VETERAN': '#e000be',
+    'RANK_MASTER': '#ff2c1a',
+    'RANK_LEGEND': '#f5b200',
+    'RANK_UNIQUE': '#b98600',
+    'QUEST_ITEM': '#666666',
 }
